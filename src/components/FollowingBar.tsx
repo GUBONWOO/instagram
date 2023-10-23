82985375e2104c0d02a02cf0d74a389ea287a23f
GUBONWOO <zcwxzsx@gmail.com>
2023-10-23 15:34:36 +0900

    following styling

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -5,29 +5,34 @@ import React from 'react';
 import { PropagateLoader } from 'react-spinners';
 import useSWR from 'swr';
 import Avatar from './Avatar';
+import ScrollableBar from './ui/ScrollableBar';
 
 export default function FollowingBar() {
   const { data, isLoading, error } = useSWR<DetailUser>('/api/me');
   const users = data?.following;
 
   return (
-    <section>
+    <section className='w-full flex justify-center items-center p-4 shadow-md shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-auto'>
       {isLoading ? (
         <PropagateLoader size={8} color='red' />
       ) : (
         (!users || users.length === 0) && <p>{`you don't have following`}</p>
       )}
       {users && users?.length > 0 && (
-        <div>
+        <ScrollableBar>
           {users.map(({ image, username }) => (
-            <li key={username}>
-              <Link href={`/user/${username}`}>
-                <Avatar image={image} highlight />
-                <p>{username}</p>
-              </Link>
-            </li>
+            <Link
+              key={username}
+              className='flex flex-col items-center w-20'
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className='w-full text-sm text-center  text-ellipsis overflow-auto'>
+                {username}
+              </p>
+            </Link>
           ))}
-        </div>
+        </ScrollableBar>
       )}
     </section>
   );
