98d758f711955b33c928217416ea41b098657119
GUBONWOO <zcwxzsx@gmail.com>
2023-10-27 14:48:18 +0900

    refactoring

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -4,11 +4,11 @@ import { SimplePost } from '@/model/post';
 import Image from 'next/image';
 import { useState } from 'react';
 import ActionBar from './ActionBar';
-import Avatar from './Avatar';
 import CommentForm from './CommentForm';
 import ModalPortal from './ui/ModalPortal';
 import PostModal from './PostModal';
 import PostDetail from './PostDetail';
+import PostUserAvatar from './PostUserAvatar';
 
 type Props = {
   post: SimplePost;
@@ -21,10 +21,7 @@ export default function PostListCard({ post, priority = false }: Props) {
 
   return (
     <article className='rounded-lg shadow-md border border-gray-200'>
-      <div className='flex items-center p-2'>
-        <Avatar image={userImage} size='medium' highlight />
-        <span className='text-gray-900 font-bold ml-2'>{username}</span>
-      </div>
+      <PostUserAvatar image={userImage} username={username} />
       <Image
         className='w-full object-cover aspect-square'
         src={image}
